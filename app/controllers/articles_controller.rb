76dca7afcb0b90c9ca800e76965b99380f86c5c5
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-16 23:02:47 -0700

    display a single article: route and erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,5 +17,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
