9ba35e2d16500358fcbf92c660d86c8bfae59d80
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-21 17:40:49 -0700

    ajax call to make new article form to appear

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,6 +1,26 @@
-$(document).ready(function() {
-
+var setEventListenerForArticleForm = function() {
   $('.new_article').on('click', function(event) {
     event.preventDefault();
-  })
+
+    var request = $.ajax({
+      url: '/articles/new',
+      type: 'get',
+    });
+
+    request.done(function(response) {
+      $('.articles_form').show();
+    });
+
+    request.fail(function() {
+      console.log("adding the form fails");
+    });
+  });
+}
+
+// var setListenerForNewArticle = function() {
+//   $('.articles_form').on('submit')
+// }
+
+$(document).ready(function() {
+  setEventListenerForArticleForm();
 });
