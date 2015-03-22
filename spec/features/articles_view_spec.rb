4db602703cac306fe860205dd21180f30856c1e7
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-21 17:39:56 -0700

    change new article link to button

--- spec/features/articles_view_spec.rb
+++ spec/features/articles_view_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Article, :js => true do
   it "has a button that links to create a new article" do
     visit articles_path
-    expect(page).to have_content('New Article')
+    expect(page).to have_button('New Article')
   end
 
   it "adds a form to the page to create a new article" do
