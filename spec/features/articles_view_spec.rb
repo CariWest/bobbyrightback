49cfc6476dbe17f90ca39590ab242fc2d9db5d96
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-21 17:41:51 -0700

    add before block for articles_path

--- spec/features/articles_view_spec.rb
+++ spec/features/articles_view_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 describe Article, :js => true do
-  it "has a button that links to create a new article" do
+
+  before(:each) do
     visit articles_path
+  end
+
+  it "has a button that links to create a new article" do
     expect(page).to have_button('New Article')
   end
 
   it "adds a form to the page to create a new article" do
-    visit articles_path
     click_button('New Article')
     expect(page).to have_selector('form')
+  end
+
+  it "can create a new article" do
   end
 
   # capybara matchers
