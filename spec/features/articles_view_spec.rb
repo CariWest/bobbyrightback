00ddb3d542c5232cda14fcdd78ad5ebf8c083cf0
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-21 17:50:24 -0700

    finalize test for new article creation

--- spec/features/articles_view_spec.rb
+++ spec/features/articles_view_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe Article, :js => true do
 
+  let!(:article) { FactoryGirl.create :article }
   before(:each) do
     visit articles_path
+  end
+
+  after(:each) do
+    Article.destroy_all
   end
 
   it "has a button that links to create a new article" do
@@ -16,6 +21,10 @@ describe Article, :js => true do
   end
 
   it "can create a new article" do
+    click_button('New Article')
+    fill_in 'Title', with: article.title
+    fill_in 'Content', with: article.content
+    click_button('Publish')
   end
 
   # capybara matchers
