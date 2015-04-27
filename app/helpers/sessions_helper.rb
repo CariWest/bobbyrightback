e1595bd9fa524c81a6fdf0a3144a28e8cc14c48d
Cari Westbrook <cari.westbrook@gmail.com>
2015-04-27 14:28:23 -0700

    test that a user is logged in vs not logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,2 @@
 module UsersHelper
-
-  def current_user
-    @current_user ||= User.find(session[:id])
-  end
-
-  def authorize
-    redirect_to '/login' unless current_user
-  end
-
 end
