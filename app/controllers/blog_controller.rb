8cea1f7f175fe3150d0c3e26ef92c70ee47ef4c6
Cari Westbrook <cari.westbrook@gmail.com>
2015-04-28 17:44:05 -0700

    set up blog controller routes

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,3 +1,25 @@
 class BlogController < ApplicationController
   before_filter :authorize
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
